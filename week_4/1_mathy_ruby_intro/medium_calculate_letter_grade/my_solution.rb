4f3576a7aa29d6ae5da9ef29aa3c2d822d9c5d60
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-12 15:55:09 -0700

    Add pseudocode for Medium Calc Letter Grade

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -12,6 +12,24 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# Create a method `get_grade` that accepts an `Array` of test scores.  Each score in the array should be between `0` and `100`, where
+# `100` is the max score.
+# Compute the average score and return the letter grade as a `String`, i.e., `'A'`, `'B'`, `'C'`, `'D'`, or `'F'`.
+
+# Define the get_grade method with (array) as its parameter.
+#   Set sum equal to 0.
+#   Iterate through Array using the each method passing a block {|index| sum is equal to sum plus index}
+#   Set average equal to the sum as a float divided by the lenght of the array.
+#   Output sum.
+#   Create a case for average.
+#     When average is between 90 and 100 then output "A".
+#     When average is between 80 and  90 then output "B".
+#     When average is between 70 and  80 then output "C".
+#     When average is between 60 and  70 then output "D".
+#     When average is between  0 and  60 then output "F".
+#     Else error.
+#   End case.
+# End method.
 
 # 2. Initial Solution
 
