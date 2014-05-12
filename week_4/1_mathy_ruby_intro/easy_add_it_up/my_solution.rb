0ecf51b51dc7aab9e362f018c7b118825b705955
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-12 14:58:40 -0700

    Edit my solution with pseudocode for Easy Add It Up

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,13 +4,20 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
+
+# Write a method `total` which takes an `Array` of numbers as its input and returns their total (sum).
+# Define the total method with (Array) as its parameter.
+# Set sum equal to 0.
+# Iterate through Array using the each method passing a block {|index| sum is equal to sum plus index}
+# Output the sum.
+# End the method.
 
 
 # 2. Initial Solution
