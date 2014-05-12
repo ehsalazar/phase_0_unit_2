6f499366fa553734b59647c0340b184a3397ffe1
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-12 15:31:08 -0700

    Add initial solution to Easy Add It Up

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -13,15 +13,37 @@
 # What are the steps needed to solve the problem?
 
 # Write a method `total` which takes an `Array` of numbers as its input and returns their total (sum).
-# Define the total method with (Array) as its parameter.
-# Set sum equal to 0.
-# Iterate through Array using the each method passing a block {|index| sum is equal to sum plus index}
-# Output the sum.
+
+# Define the total method with (array) as its parameter.
+#   Set sum equal to 0.
+#   Iterate through Array using the each method passing a block {|index| sum is equal to sum plus index}
+#   Output the sum.
 # End the method.
 
+# Then, write another method `sentence_maker` which takes an `Array` of strings or numbers and returns 
+# each element joined into a sentence. 
+# Note: The first letter should be capitalized and the sentence should end with a period.
+
+# Define the sentence_maker method with (array) as its parameter.
+#   Capitalize the first string in the array.
+#   Concatenate a period to the last string in the array.
+#   Use the join method on the array with a space parameter.
+#   Output the new sentence.
+# End the method.
 
 # 2. Initial Solution
 
+def total(array)
+  sum = 0
+  array.each {|i| sum += i}
+  sum
+end
+
+def sentence_maker(array)
+  array[0].capitalize!
+  array[array.length - 1] << "."
+  array.join(" ")
+end
 
 
 
