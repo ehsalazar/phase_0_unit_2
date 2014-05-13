0f7cb4f881f898a33719067bef114a14370b2f7d
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-13 11:48:19 -0700

    Add pseudocode for Find Mode

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -16,7 +16,18 @@
 # If there's only one most-frequent value, it returns a single-element `Array`.
 
 # Define the mode method with (array) as its parameter.
-#
+#   Create count as a new Hash set to 0
+#   Loop through the array using the each method and do |index|
+#     count[index] is equal to count[index] plus 1
+#   End loop
+#   Create mode_array set equal to an empty array.
+#   Loop through the count hash using the each method and do |key, value|
+#     If the value is equal to the count values max
+#       Shovel the key into the mode_array
+#     End if statement
+#   End loop
+#   Output the mode_array sorted by highest occurance.
+# End method 
 
 
 # 2. Initial Solution
