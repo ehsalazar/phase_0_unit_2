050cf3024b7da2f85b9ef0c728b24c3007fb7ed7
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-14 20:50:03 -0700

    Add reflection for Pad Array

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -52,3 +52,18 @@ end
 
 # 4. Reflection 
 
+# I worked with a pair on this challenge. I thought it would help to make things clearer. While it did, our actual pair
+# programming session could barely get beyond one passed test. We were on the right track however and it just took a bit
+# more thinking and exploring to solve the challenge. 
+
+# The pieces came together for the destructive method but the non-destructive method was trickier. I kept getting the same
+# error or fail test. I googled a bit and figured out that the problem was that despite what I was trying to output, the
+# original array had been permanently changed. I learned that what I needed was to make a duplicated or clone of the 
+# original array and then call the destructive method on it. That way, the output would match the original array but would 
+# in fact be a whole new object.
+
+# I'm confident in the Learning Competencies, especially after working and learning more about destructive and non-destructive.
+# I continue to get benefit out of the testing process but some question did arise during my pair programming session. After
+# running the test and seeing what it's looking for, is it better to fully flesh out an initial solution or rather build it
+# a small component at a time. For example, if the first failed test looks only for a method, then build an empty method.
+# Test and hopefully pass then go onto the next failed test and build to that, and so on.
