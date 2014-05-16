e870207dbb800d8a398333140fa0d14a17c449c9
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-16 13:46:51 -0700

    Add pseudocode forNumbers with Commas

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -7,10 +7,22 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? A random integer
+# What is the output? (i.e. What should the code return?) A comma-seperated integer, i.e., 1,000,000
 # What are the steps needed to solve the problem?
 
+# Define separate_comma method that takes one parameter (integer)
+#   num_to_string is equal to integer converted to a string
+#   length is equal to num_to_string's length
+#   if length is greater than 3
+#     index is equal to 1
+#     until index is greater than (length minus 1) divided by 3
+#       num_to_string is inserted ((at position length-(3 times index)), "comma")
+#       index is equal to index plus one
+#     end until loop
+#   end if statement
+#   return num_to_string
+# End
 
 # 2. Initial Solution
 
