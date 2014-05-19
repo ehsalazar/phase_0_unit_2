5fb7178230c18ac9a3e4486916eedd5376d9f3ce
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-19 16:13:43 -0700

    Complete part 1 for Boggle Board

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -1,6 +1,6 @@
 # U2.W5: A Nested Array to Model a Boggle Board
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 boggle_board = [["b", "r", "a", "e"],
                 ["i", "o", "d", "t"],
@@ -12,16 +12,32 @@ boggle_board = [["b", "r", "a", "e"],
 
 # Pseudocode
 
+# Use provided method
+# Understand how it works
+
 # Initial Solution
 
+def create_word(board, *coords)
+  coords.map { |coord| board[coord.first][coord.last]}.join("")  
+end
 
 # Refactored Solution
 
+# None needed
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3]) == "code"					# => true 
+puts create_word(boggle_board, [0,1], [0,2], [1,2]) == "rad"							# => true
+puts create_word(boggle_board, [3,0], [3,1], [2,2], [3,2]) == "talk"					# => true
+puts create_word(boggle_board, [2,2], [1,1], [2,1], [3,1], [3,0], [2,0]) == "locate"	# => true
+puts create_word(boggle_board, [0,1], [1,1], [2,1], [3,2], [3,3], [2,3]) == "rocker"	# => true
 
 # Reflection 
+
+# Makes sense. The create_word method takes a minimum of two parameters (the board, the coordinates). The map method then
+# loops thought the inputted coordinates and creates a new array starting with the first coordinate and finishing with the
+# last. The join method takes this new array and outputs a string.
 
 #-------------------------------------------------------------------------------
 
