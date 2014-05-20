c902dfcf04c870e84ebdb0f1022ddb7bd2197ddf
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-19 23:09:51 -0700

    Edit comments for Virus Predictor

--- week_5/5_virus_predictor/original_code.rb
+++ week_5/5_virus_predictor/original_code.rb
@@ -8,8 +8,8 @@
 # other file is needed to complete the work. For example, I can require_relative a challenge from last week to make
 # the numbers easier to read.
 
-# The state data is held in a nested hash. The state name as a string key has a hash as its value. The data hash is made up of 
-# symbols their values.
+# The state data is held in a nested hash. The state name as a string key has a hash as its value. The data hash is 
+# made up of symbols their values.
 
 require_relative 'state_data'
 require_relative '../../week_4/4_nums_commas_solo_challenge/my_solution'
@@ -24,21 +24,24 @@ class VirusPredictor
     @next_region = regional_spread
   end
 
-  # This is a public instance method of the class. Each state or member of the class is initialized with instance variables. 
-  # Because they are instance variables, they likely don't need to be included as parameters in any of the methods, either public 
-  # or private. Each state or member can access the methods only with their specific variables. 
+  # This is a public instance method of the class. Each state or member of the class is initialized with instance 
+  # variables. Because they are instance variables, they likely don't need to be included as parameters in any of 
+  # the methods, either public or private. Each state or member can access the methods only with their specific 
+  # variables. 
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
-  # Public methods allow for an interface with the program. If you need to access something about the class or its instances, 
-  # it happens here. On the other hand, private methods are to be undisturbed. They are usually unreachable. For example, 
-  # a bank program might have public methods to deal with account names and types but private methods that store and update pin numbers. 
+  # Public methods allow for an interface with the program. If you need to access something about the class or its 
+  # instances, it happens here. On the other hand, private methods are to be undisturbed. They are usually 
+  # unreachable. For example, a bank program might have public methods to deal with account names and types but 
+  # private methods that store and update pin numbers. If I move "private" above the virus_effect method, you will
+  # be unable to call it from outside the class.
   private 
 
-  # This method takes the state's population data and calculates how many deaths could occur. The higher the population density 
-  # the more deaths.
+  # This method takes the state's population data and calculates how many deaths could occur. The higher the 
+  # population density the more deaths.
   def predicted_deaths                                                           
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -54,8 +57,8 @@ class VirusPredictor
     print "#{@state} will lose #{seperate_comma(number_of_deaths)} people in this outbreak"
   end
 
-  # This method takes the state's population data and calculates how quickly the virus will spread. The output is in month or 
-  # fraction thereof. The higher the population density, the faster it will spread.
+  # This method takes the state's population data and calculates how quickly the virus will spread. The output is 
+  # in months or fraction thereof. The higher the population density, the faster it will spread.
   def speed_of_spread  
     speed = 0.0
 
@@ -80,9 +83,9 @@ end
 # Refactored ===========================================================
 
 class VirusPredictor
-  # I automated the initialization method to pull in the data from the STATE_DATA hash. Now all that's needed to initialize 
-  # a new member of the class is the state name. For example: alabama = VirusPredictor.new("Alabama") You can then call 
-  # alabama.virus_effect to get the individual state's report.
+  # I automated the initialization method to pull in the data from the STATE_DATA hash. Now all that's needed 
+  # to initialize a new member of the class is the state name. For example: alabama = VirusPredictor.new("Alabama") 
+  # You can then call alabama.virus_effect to get the individual state's report.
   def initialize(state)
     @data = STATE_DATA[state]
     @state = state
@@ -97,8 +100,8 @@ class VirusPredictor
   end
 
   private
-  # The predicted_deaths and speed_of_spread were both calculating based on @population_density. As such, they can be combined
-  # into one cleaner method.
+  # The predicted_deaths and speed_of_spread were both calculating based on @population_density. As such, they can 
+  # be combined into one cleaner method.
   def impact    
     speed = 0.0
     case 
@@ -118,7 +121,8 @@ class VirusPredictor
         number_of_deaths = (@population * 0.05).floor
         speed += 2.5
       end
-    puts "#{@state} will lose #{separate_comma(number_of_deaths)} people in this outbreak and it will spread across the state in #{speed} months.\n\n"
+    puts "#{@state} will lose #{separate_comma(number_of_deaths)} people in this outbreak and it will spread across 
+    the state in #{speed} months.\n\n"
   end
 
 end
@@ -130,10 +134,12 @@ end
 # Initialize VirusPredictor for each state one at a time
 
 alabama = VirusPredictor.new("Alabama")
-alabama.virus_effects  # => "Alabama will lose 482,202 people in this outbreak and it will spread across the state in 2.0 months."
+alabama.virus_effects  
+# => "Alabama will lose 482,202 people in this outbreak and it will spread across the state in 2.0 months."
 
 nevada = VirusPredictor.new("Nevada")
-nevada.virus_effects   # => "Nevada will lose 137,946 people in this outbreak and it will spread across the state in 2.5 months."
+nevada.virus_effects   
+# => "Nevada will lose 137,946 people in this outbreak and it will spread across the state in 2.5 months."
 
 # Initialize VirusPredictor for all states
 
