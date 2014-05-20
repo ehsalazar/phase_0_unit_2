cac89e252ccdfbbf3f1061f1eaea2b02905fe0da
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-19 17:00:54 -0700

    Complete parts 2 and 3 for Boggle Board

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -45,18 +45,30 @@ puts create_word(boggle_board, [0,1], [1,1], [2,1], [3,2], [3,3], [2,3]) == "roc
 
 # Pseudocode
 
+# Define the get_row method that takes two parameters (the board, the row)
+#   return index [row] of board array
+# end get_row method
+
 # Initial Solution
 
+def get_row(board, row)
+  board[row]
+end
 
 # Refactored Solution
 
+# None needed
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+p get_row(boggle_board, 1) == ["i", "o", "d", "t"] 	# => true
+p get_row(boggle_board, 4) == nil					# => true
+p get_row(boggle_board, 0) == ["b", "r", "a", "e"]	# => true
 
 # Reflection 
 
-
+# By having both the board and row as parameters, the get_row method can be super simple. You are basically just asking for
+# a specific index of the array. 
 
 #-------------------------------------------------------------------------------
 
@@ -66,11 +78,30 @@ puts create_word(boggle_board, [0,1], [1,1], [2,1], [3,2], [3,3], [2,3]) == "roc
 
 # Initial Solution
 
+def get_col(board, col)
+  board.map {|arr| arr[col]}
+end
 
 # Refactored Solution
 
+# None needed
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+p get_col(boggle_board, 3) == ["e", "t", "r", "e"]  # => true
+p get_col(boggle_board, 6) == [nil, nil, nil, nil]  # => true
+p get_col(boggle_board, 0) == ["b", "i", "e", "t"]  # => true
 
 # Reflection 
+
+# Having worked with arrays last week made this challenge that much simpler. I will say however that I went off the README
+# instructions slightly. I used two parameters, a board and either a row or column for the second and third functions. Without
+# the board parameter, you would have to set a global variable within the method to access the original array. That would
+# then greatly limit the usability of the code. With two parameters, the methods can be used on any board the player was using.
+
+# I didn't stray from the instructions on the numbering of rows and columns. As such, the first row is actually row 0. If 
+# you wanted to make the methods more intuitive for a non-programming person, you could easily adjust. So that if get_row(1)
+# was called, it could return array[0] rather than array[1]. 
+
+# Overall this was a fun challenge. It wasn't overly complicated but it also had us working on something I could logically 
+# associate with outside of programming. I feel confident in the Learning Competencies and found no aspect tedious. 
