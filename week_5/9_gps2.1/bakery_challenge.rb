e7c62a64972f18b8bc989891c78a7a60a06b2cb8
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-23 16:35:34 -0700

    Add reflection forBakery Challenge

--- week_5/9_gps2.1/bakery_challenge.rb
+++ week_5/9_gps2.1/bakery_challenge.rb
@@ -1,50 +1,61 @@
 # U2.W5: The Bakery Challenge (GPS 2.1)
 
 # Your Names
-# 1)
-# 2)
+# 1) Darrell Jones
+# 2) Ernie Salazar
 
- # This is the file you should end up editing. 
+# 1. Input: The number of people to feed and thier favorite food
+# 2. Output: The quantity of food items to make. 
+
+# This is the file you should end up editing. 
  
-def bakery_num(num_of_people, fav_food)
-  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
-  pie_qty = 0
+def bakery_num(num_of_people, fav_food) # define method with 2 parameters
+  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1} # hash with keys of items 
+                                                     # and value of number of people it feeds.
+  pie_qty = 0 
   cake_qty = 0
-  cookie_qty = 0
+  cookie_qty = 0 # the food items available set to zero
   
-  has_fave = false
+  has_fave = false # Does the bakery make this item?
 
-  my_list.each_key do |k|
-  if k == fav_food
-  has_fave = true
-  fav_food = k
+  my_list.each_key do |k| # determines if bakery makes said item. If so, has_fave == true
+    if k == fav_food
+      has_fave = true
+      fav_food = k
+    end
   end
-  end
-  if has_fave == false
-  raise ArgumentError.new("You can't make that food")
+  
+  if has_fave == false # if we don't make that food, raise error
+    raise ArgumentError.new("You can't make that food")
   else
-  fav_food_qty = my_list.values_at(fav_food)[0]
-  if num_of_people % fav_food_qty == 0
-    num_of_food = num_of_people / fav_food_qty
-    return "You need to make #{num_of_food} #{fav_food}(s)."
-  else num_of_people % fav_food_qty != 0
-    while num_of_people > 0
-    if num_of_people / my_list["pie"] > 0
-    pie_qty = num_of_people / my_list["pie"]
-    num_of_people = num_of_people % my_list["pie"]
-    elsif num_of_people / my_list["cake"] > 0
-    cake_qty = num_of_people / my_list["cake"]
-    num_of_people = num_of_people % my_list["cake"]
-    else
-    cookie_qty = num_of_people
-    num_of_people = 0
-    end
-    end
+    fav_food_qty = my_list.values_at(fav_food)[0] # determines the amount of people are feed
+                                                  # by fav_food
+      if num_of_people % fav_food_qty == 0 # If the number of people is equally divisible by
+                                            # the food item, then divide the number of people
+                                            # by the food qty to output the number of food 
+                                            # items to make.
+        num_of_food = num_of_people / fav_food_qty
+        return "You need to make #{num_of_food} #{fav_food}(s)."
+      else num_of_people % fav_food_qty != 0 # If the number of people is not equally divisible
+                                            # although else doesnt need statement
+                                            
+        while num_of_people > 0                 # while loop to determine the foods to make
+          if num_of_people / my_list["pie"] > 0 # to feed the largest quantity of people
+                                                # and make cookies for the balance.
+            pie_qty = num_of_people / my_list["pie"]
+            num_of_people = num_of_people % my_list["pie"]
+          elsif num_of_people / my_list["cake"] > 0
+            cake_qty = num_of_people / my_list["cake"]
+            num_of_people = num_of_people % my_list["cake"]
+          else
+            cookie_qty = num_of_people
+            num_of_people = 0
+          end
+        end
     return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
-    end
+    end  # the output is a string with the quantities of each item to make filled in. 
   end
 end
- 
 
 #-----------------------------------------------------------------------------------------------------
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -1,20 +1,34 @@
 # U2.W5: Bakery Challenge GPS
 
-# I worked on this challenge with: 
-
-
-
-
-
+# I worked on this challenge with Darrell Jones
 
 # Our Refactored Solution
 
+def bakery_num(people, food) 
+  menu = {"pie" => 8, "cake" => 6, "cookie" => 1}
+  qty = {"pie" => 0, "cake" => 0, "cookie" => 0}
+  
+  raise ArgumentError.new("You can't make that food") if menu.has_key?(food) == false
 
-
-
-
-
-
+  if people % menu[food] == 0 
+    food_qty = people / menu[food]
+    "You need to make #{food_qty} #{food}(s)."
+  else
+    while people > 0                 
+      if people / menu["pie"] > 0 
+        qty["pie"] = people / menu["pie"]
+        people = people % menu["pie"]
+      elsif people / menu["cake"] > 0
+        qty["cake"] = people / menu["cake"]
+        people = people % menu["cake"]
+      else
+        qty["cookie"] = people
+        people = 0
+      end
+    end
+    "You need to make #{qty["pie"]} pie(s), #{qty["cake"]} cake(s), and #{qty["cookie"]} cookie(s)."
+  end	
+end
 
 
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
@@ -25,15 +39,22 @@ p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s
 p bakery_num(24, "cookie") == "You need to make 24 cookie(s)."
 p bakery_num(4, "pie") == "You need to make 0 pie(s), 0 cake(s), and 4 cookie(s)."
 p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie(s)."
-# p bakery_num(3, "apples") # this will raise an ArgumentError
+#p bakery_num(3, "apples") # this will raise an ArgumentError
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
-p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
-
-
-
+p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." 
 
 #  Reflection 
 
+# I enjoyed this challenge. At first my GPS partner and I were a little confused by the bulky original code but once
+# we figured out what was going on, we off and running. A big take away from the challenge was to fully read and
+# understand the original or legacy code prior to trying to jump in and comment, refactor or change it. 
 
+# To start, I was a little confused by the original while loop. Talking it through helped. I realized it was doing 
+# what it was intended to do, not necessarily what I thought it should do. I made the assumption that if the favorite 
+# food was among those the baker made, that if the group was not divisible by the number of people that food fed, 
+# that the while loop would output the favorite food first followed by whatever else would be needed to feed the 
+# group. In reality, it always outputs pies first since that feeds the most people.
 
+# I'm confident in this challenges Learning Competencies. I got a good amount of learning out of the challenge. Both
+# because of the challenge, but also thanks to my GPS partner. Great experience all the way around.  
